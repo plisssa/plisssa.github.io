ab9afd046413a8568f07f0de155d58794c3fa725
plisssa <90327013+plisssa@users.noreply.github.com>
2022-07-04 01:35:46 +0300

    Add files via upload

---
+++ js/1.js
@@ -0,0 +1,10 @@
+$('[href^="#"]').on('click', function(){
+    let href = $(this).attr('href'), elem = $(document).find(href);
+    if(elem.length > 0) {
+      let posY = elem.eq(0).offset().top;
+      $('html, body').animate({
+        scrollTop: posY
+      }, 1000);
+    }
+    return false;
+  });
